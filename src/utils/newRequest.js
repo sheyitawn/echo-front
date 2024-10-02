260ca1793937e011a381c9c584b52a81e8d0eecd
sheyitawn <randombuzzer234@gmail.com>
2024-10-02 17:06:23 +0100

    base url name

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -5,7 +5,10 @@ import { ToastContainer, toast } from 'react-toastify';
 // Base URL setup
 const baseURL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:3001/api'  // Development URL
-  : process.env.API_URL;  // Production URL
+  : process.env.BACKEND_URL;  // Production URL
+
+
+console.log("🚀 ~ baseURL:", baseURL)
 
 // Create an Axios instance
 const newRequest = axios.create({
